8a678c23ce123c20c4cbbb9aa6035a057bc78d78
ProFrenchToast <66527432+ProFrenchToast@users.noreply.github.com>
2021-02-24 22:16:46 +0000

    Implemented rabbit movement.

--- Environments/HuntingRabbits.py
+++ Environments/HuntingRabbits.py
@@ -88,6 +88,8 @@ class HuntingRabbits(gym.Env):
         self.height = height
         self.num_rabbits = num_rabbits
         self.sickness_prob = sickness_prob
+        self.speed_change_prob = speed_change_prob
+        self.rabbit_sight = sight
 
         self.world = generate_rabbit_world(width, height, num_rabbits)
         self.rabbits_caught = 0
@@ -152,10 +154,68 @@ class HuntingRabbits(gym.Env):
         raise NotImplementedError
 
     def move_rabbits(self):
-        # todo: implement random rabbit movement
+        # get a list of all the rabbits
+        rabbit_list = []
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.world[x, y] == CellTypes.Rabbit_1 or self.world[x, y] == CellTypes.Rabbit_2 or self.world[x, y] == CellTypes.Rabbit_3:
+                    rabbit_list.append((x, y))
 
-        # todo: implement player avoidance
-        pass
+        for rabbit in rabbit_list:
+            destination = rabbit        #default to not moving
+
+            # first randomly change the speed of the rabbits
+            if random.random() < self.speed_change_prob:
+                # Note: the rabbits change to any possible speed rather than just adjacent speeds because otherwise they
+                # would be unevenly distributed.
+                self.world[rabbit[0], rabbit[1]] = random.randrange(CellTypes.Rabbit_1, CellTypes.Rabbit_3, 1)
+
+            # check to see if player is within sight range
+            player = find_player(self.world)
+            difference = (abs(player[0] - rabbit[0]), abs(player[1] - rabbit[1]))
+            if max(difference) <= self.rabbit_sight:
+                # run away (player is close)
+                speed = 0
+                if self.world[rabbit[0], rabbit[1]] == CellTypes.Rabbit_1:
+                    speed = 1
+                elif self.world[rabbit[0], rabbit[1]] == CellTypes.Rabbit_2:
+                    speed = 2
+                elif self.world[rabbit[0], rabbit[1]] == CellTypes.Rabbit_3:
+                    speed = 3
+                else:
+                    raise Exception("No rabbit found at ({}, {})".format(rabbit))
+
+                max_dist_from_player = 0
+                max_dist_cells = [rabbit]   # the rabbit covers the case where the rabbit cannot move but can see player
+
+                for x in range(-speed, speed, 1):
+                    for y in range(-speed, speed, 1):
+                        possible_destination = ((rabbit[0] + x), (rabbit[1] + y))
+
+                        if possible_destination[0] < 0 or possible_destination[0] >= self.width or \
+                                possible_destination[1] < 0 or possible_destination[1] >= self.height:
+                            continue
+                        elif self.world[possible_destination[0], possible_destination[1]] == CellTypes.Empty:
+                            distance_to_player = max(abs(player[0] - possible_destination[0]),
+                                                         abs(player[1] - possible_destination[1]))
+                            if distance_to_player > max_dist_from_player:
+                                max_dist_from_player = distance_to_player
+                                max_dist_cells = [possible_destination]
+                            elif distance_to_player == max_dist_from_player:
+                                max_dist_from_player.append[possible_destination]
+
+                # randomly pick from the best cells
+                destination = random.choice(max_dist_cells)
+
+            else:
+                # move randomly (player is far away)
+                destination = ((rabbit[0] + random.randrange(-1, 1, 1)), (rabbit[1] + random.randrange(-1, 1, 1)))
+
+            rabbit_type = self.world[rabbit[0], rabbit[1]]
+            self.world[rabbit[0], rabbit[1]] = CellTypes.Empty
+            self.world[destination[0], destination[1]] = rabbit_type
+
+        return
 
 
 class CellTypes(enum.IntEnum):
