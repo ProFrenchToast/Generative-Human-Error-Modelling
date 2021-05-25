Fixed training loop and made TimeMaze actions have gradient.

GHEM/Environments/TimeMaze.py
@@ -25,10 +25,11 @@ def generate_maze(width, height):
     # select a random starting point
     start_x = random.randrange(0, width, 1)
     start_y = random.randrange(0, height, 1)
-    maze[start_x, start_y] = CellTypes.Player
 
     # now randomly build the maze
     random_breath_search(maze, (start_x, start_y))
+
+    maze[start_x, start_y] = CellTypes.Player
 
     # randomly place the goal
     goal_placed = False
@@ -48,14 +49,6 @@ def generate_maze(width, height):
             maze[stopwatch_x, stopwatch_y] = CellTypes.Stopwatch
             stopwatch_placed = True
 
-    # randomly place the player
-    player_placed = False
-    while not player_placed:
-        player_x = random.randrange(0, width, 1)
-        player_y = random.randrange(0, height, 1)
-        if maze[player_x, player_y] == CellTypes.Empty:
-            maze[player_x, player_y] = CellTypes.Player
-            player_placed = True
 
     return maze
 
@@ -171,6 +164,8 @@ class TimeMaze(gym.Env):
 
     def __init__(self, seed=None, width=30, height=30):
         self.action_space = gym.spaces.Discrete(4)
+        # actions "should" be discrete but the env will automatically round it to the nearest int.
+        # This is done so that agents can have actions that are differentiable even in a discrete env
         self.observation_space = gym.spaces.Box(low=0, high=max(CellTypes), shape=(width, height), dtype=int)
         self.initial_seed = seed
         if seed is not None:
@@ -246,6 +241,9 @@ class TimeMaze(gym.Env):
             reward = 0
             self.done = True
             return self.maze, reward, self.done, {}
+
+        # round non-int actions and validate they are in the right range
+        action = np.rint(action)
 
         player_cell = find_player(self.maze)
 

GHEM/Models/TimeMazeModels.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 import numpy as np
 
-from BaseModels import *
+from GHEM.Models.BaseModels import *
 
 import torch.nn as nn
 import torch
@@ -74,8 +74,7 @@ class TimeMazeGenerator(BaseGenerator):
         fc_blocks += [nn.Linear(previous_layer_size, next_layer_size),
                       nn.Dropout(0.5)]
 
-        fc_blocks += [nn.Linear(next_layer_size, 4),
-                      nn.Softmax()]
+        fc_blocks += [nn.Linear(next_layer_size, 1)]
         self.fc_blocks = nn.Sequential(*fc_blocks)
 
     def forward(self, error_vector, observation):
@@ -85,6 +84,7 @@ class TimeMazeGenerator(BaseGenerator):
         combined = torch.cat((conv_results_flattened, error_vector_flattened), 0)
         fc_results = self.fc_blocks(combined)
         return fc_results
+
 
 
 class TimeMazeDiscriminator(BaseDiscriminator):
@@ -150,14 +150,16 @@ class TimeMazeDiscriminator(BaseDiscriminator):
         fc_blocks += [nn.Linear(previous_layer_size, next_layer_size),
                       nn.Dropout(0.5)]
 
-        fc_blocks += [nn.Linear(next_layer_size, 1)]
+        fc_blocks += [nn.Linear(next_layer_size, 1),
+                      nn.Softmax()]
         self.fc_blocks = nn.Sequential(*fc_blocks)
 
     def forward(self, error_vector, observation, action):
         conv_results = self.conv_blocks(observation)
         conv_results_flattened = torch.flatten(conv_results)
         error_vector_flattened = torch.flatten(error_vector)
-        combined = torch.cat((conv_results_flattened, error_vector_flattened, action), 0)
+        action_flattened = torch.flatten(action)
+        combined = torch.cat((conv_results_flattened, error_vector_flattened, action_flattened), 0)
         fc_results = self.fc_blocks(combined)
         return fc_results
 

GHEM/Utils/BaseAgent.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+
+
 class Agent(object):
     """The generic class to hold all types of agents"""
 
@@ -26,12 +30,16 @@ class RandomAgent(Agent):
 
 class AgentWrapper(Agent):
     """A wrapper for generators that lets them act like agents"""
-    def __init__(self, env, model, error_vector):
+    def __init__(self, env, model, error_vector, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
         self.model = model
-        self.error_vector = error_vector
+        self.device = device
+        self.error_vector = torch.from_numpy(np.array(error_vector)).float().to(device)
 
     def act(self, observation, reward, done):
-        return self.model.forward(self.error_vector, observation)
+        obs_tensor = torch.from_numpy(observation).float().to(self.device)
+        obs_tensor = obs_tensor.unsqueeze(0)
+        obs_tensor = obs_tensor.unsqueeze(0)
+        return self.model(self.error_vector, obs_tensor)
 
     def reset(self, env):
         pass

GHEM/Utils/TrainModels.py
@@ -1,10 +1,12 @@
 from GHEM.Models.BaseModels import BaseGenerator, BaseDiscriminator
 from GHEM.Utils.ErrorVector import BaseErrorVectorGen
 from GHEM.Utils.GenerateDemos import generate_fake_demos
+import torch
 
 
 def fixed_alternation_training(epochs, demonstrations, error_vector_gen, generator, discriminator, gen_optim, disc_optim
-                               , env, gen_loss, disc_loss, sub_epochs=1, mini_batch_size=1):
+                               , env, gen_loss, disc_loss, sub_epochs=1, mini_batch_size=1,
+                               device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
     assert mini_batch_size >= 1
     assert sub_epochs >= 1
     assert epochs >= 1
@@ -25,22 +27,31 @@ def fixed_alternation_training(epochs, demonstrations, error_vector_gen, generat
 
             for example in range(mini_batch_size):
                 # variable prefixed with real_ or fake_ refer to if they were created by the demonstrator or generator
-                real_demo, real_total_reward, real_error_vector = demonstrations.choice()
-                real_state, real_action, real_reward = real_demo.choice()
-                prediction_real = discriminator.forward(real_error_vector, real_state, real_action)
-                disc_loss_real = disc_loss(prediction_real, True)
+                real_demo, real_total_reward, real_error_vector = random.choice(demonstrations)
+                real_state, real_action, real_reward = random.choice(real_demo)
+                real_state_tensor = torch.from_numpy(np.array(real_state)).float().to(device)
+                real_state_tensor = real_state_tensor.unsqueeze(0)
+                real_state_tensor = real_state_tensor.unsqueeze(0)
+                real_action_tensor = torch.from_numpy(np.array(real_action)).float().to(device)
+                real_error_vector_tensor = torch.from_numpy(np.array(real_error_vector)).float().to(device)
+                prediction_real = discriminator.forward(real_error_vector_tensor, real_state_tensor, real_action_tensor)
+                disc_loss_real = disc_loss(prediction_real, BaseDiscriminator.real_label)
                 current_disc_loss += disc_loss_real
 
-                fake_demo, fake_total_reward, fake_error_vector = fake_demos.choice()
-                fake_state, fake_action, fake_reward = fake_demo.choice()
+                fake_demo, fake_total_reward, fake_error_vector = random.choice(fake_demos)
+                fake_state, fake_action, fake_reward = random.choice(fake_demo)
                 # need to detach the action from the generator so it isn't updated (might need to deepcopy)
                 detached_action = fake_action.detach()
-                prediction_fake = discriminator.forward(fake_error_vector, fake_state, detached_action)
-                disc_loss_fake = disc_loss(prediction_fake, False)
+                fake_state_tensor = torch.from_numpy(np.array(fake_state)).float().to(device)
+                fake_state_tensor = fake_state_tensor.unsqueeze(0)
+                fake_state_tensor = fake_state_tensor.unsqueeze(0)
+                fake_error_vector_tensor = torch.from_numpy(np.array(fake_error_vector)).float().to(device)
+                prediction_fake = discriminator.forward(fake_error_vector_tensor, fake_state_tensor, detached_action)
+                disc_loss_fake = disc_loss(prediction_fake, BaseDiscriminator.fake_label)
                 current_disc_loss += disc_loss_fake
 
             current_disc_loss = current_disc_loss / (mini_batch_size * 2)
-            current_disc_loss.backwards()
+            current_disc_loss.backward()
             disc_optim.step()
 
         # disable the gradient tracking on the discriminator so it isn't updated
@@ -48,15 +59,59 @@ def fixed_alternation_training(epochs, demonstrations, error_vector_gen, generat
         # now train the generator using the updated discriminator
         current_gen_loss = 0
         for example in range(mini_batch_size):
-            fake_demo, fake_total_reward, fake_error_vector = fake_demos.choice()
-            fake_state, fake_action, fake_reward = fake_demo.choice()
-            prediction_fake = discriminator.forward(fake_error_vector, fake_state, fake_action)
-            current_gen_loss += gen_loss(prediction_fake)
+            fake_demo, fake_total_reward, fake_error_vector = random.choice(fake_demos)
+            fake_state, fake_action, fake_reward = random.choice(fake_demo)
+            fake_state_tensor = torch.from_numpy(np.array(fake_state)).float().to(device)
+            fake_state_tensor = fake_state_tensor.unsqueeze(0)
+            fake_state_tensor = fake_state_tensor.unsqueeze(0)
+            fake_error_vector_tensor = torch.from_numpy(np.array(fake_error_vector)).float().to(device)
+            prediction_fake = discriminator.forward(fake_error_vector_tensor, fake_state_tensor, fake_action)
+            current_gen_loss += gen_loss(prediction_fake, BaseDiscriminator.real_label)
 
         current_gen_loss = current_gen_loss / (mini_batch_size * 2)
-        current_gen_loss.backwards()
+        current_gen_loss.backward()
         gen_optim.step()
 
+        # re-enable gradient tracking for the discriminator
+        discriminator.set_req_grad(True)
 
+
+if __name__ == "__main__":
+    import random
+    random.seed(6321464)
+    import torch
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    from GHEM.Environments.TimeMaze import TimeMaze
+    env = TimeMaze()
+    from GHEM.Human_like_agents.TimeMazeAgents import *
+    agent_classes = [PrioritiseGoalAgent, PrioritiseStopwatchAgent, GeneralDirectionAgent]
+    episodes = [2, 2, 2]
+    from GHEM.Utils.GenerateDemos import generate_demonstrations_from_classes, combine_errors_and_demos
+    demos = generate_demonstrations_from_classes(env, agent_classes, episodes)
+    from GHEM.Utils.ErrorVector import error_inverse_reward
+    error_labels = error_inverse_reward(demos, env, agent_classes, episodes)
+    combined = combine_errors_and_demos(demos, error_labels)
+
+    epochs = 10
+    demonstrations = combined
+    from GHEM.Utils.ErrorVector import RandRangeErrorGen
+    error_vector_gen = RandRangeErrorGen(0, 1, 1)
+    from GHEM.Models.TimeMazeModels import TimeMazeGenerator, TimeMazeDiscriminator
+    generator = TimeMazeGenerator(30, 30, [1])
+    discriminator = TimeMazeDiscriminator(30, 30, [1])
+
+    generator.to(device)
+    discriminator.to(device)
+    learning_rate = 0.001
+    weight_decay = 0.001
+    gen_optim = torch.optim.Adam(generator.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    disc_optim = torch.optim.Adam(discriminator.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    gen_loss = torch.nn.BCELoss()
+    disc_loss = torch.nn.BCELoss()
+
+    fixed_alternation_training(epochs, demonstrations, error_vector_gen, generator, discriminator, gen_optim, disc_optim, env, gen_loss, disc_loss)
+    print("it worked")
 
 